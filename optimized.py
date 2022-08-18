92648236aca9851aa0f0eb8f47f10a57a37863fa
B-asile <84376166+B-asile@users.noreply.github.com>
2022-08-18 09:03:03 +0200

    refonte analyse actions.csv

--- optimized.py
+++ optimized.py
@@ -6,11 +6,11 @@ import time
 
 
 def file_choice():
+    print("choisissez un fichier à analyser :\n"
+          "1 - pour actions.csv\n"
+          "2 - pour dataset1_Python+P7.csv\n"
+          "3 - pour dataset1_Python+P7.csv\n")
     while True:
-        print("choisissez un fichier à analyser :\n"
-                "1 - pour actions.csv\n"
-                "2 - pour dataset1_Python+P7.csv\n"
-                "3 - pour dataset1_Python+P7.csv\n")
         user_select = input("saisissez le N° correspondant au fichier de votre choix:")
         if user_select == "1" or user_select == "2" or user_select == "3":
             return user_select
@@ -25,29 +25,24 @@ def file_choice():
 #         self.action_benefit = action_benefit
 
 
-def transform_csv_to_obj(choice):
+def transform_csv_to_obj(file):
     list_actions = []
     # max_invest = 0
 
-    if choice == "1":
+    if file == "1":
         with open('actions.csv') as file:
             obj_csv = csv.DictReader(file, delimiter=',')
             # next(obj_csv)
             for row in obj_csv:
-                # action = (row["action_name"], int(row["action_price"]), int(row["action_profit"]))
-                action = (row["name"], int(row["price"]), int(row["profit"]))
+                action = (row["name"], int(row["price"]), int(row["profit"]), int(row["profit"]) * int(row["price"]))
                 list_actions.append(action)
-                # benefit = (int(row["price"]) * int(row["profit"]))
-                benefit = int(list_actions.price) * int(list_actions.profit)
-                list_actions.append(benefit)
-                print(list_actions)
             max_invest = 500
     else:
-        if choice == "2":
+        if file == "2":
             with open('dataset1_Python+P7.csv') as file:
                 obj_csv = csv.reader(file, delimiter=',')
                 next(obj_csv)
-        elif choice == "3":
+        elif file == "3":
             with open('dataset2_Python+P7.csv') as file:
                 obj_csv = csv.reader(file, delimiter=',')
                 next(obj_csv)
@@ -58,16 +53,16 @@ def transform_csv_to_obj(choice):
     return list_actions, max_invest
 
 
-# O(n^W)
+# O(NW)
 def optimized_investment(max_invest, list_actions):
     matrice = [[0 for x in range(max_invest + 1)] for x in range(len(list_actions) + 1)]
 
     for i in range(1, len(list_actions) + 1):
         for w in range(1, max_invest + 1):
-            if int(list_actions[i-1].action_price) <= w:
-                matrice[i][w] = max((list_actions[i-1].action_benefit) + matrice[i-1]
+            if int(list_actions[i-1][1]) <= w:
+                matrice[i][w] = max((list_actions[i-1][3]) + matrice[i-1]
                # matrice[i][w] = max(int(list_actions[i - 1].action_benefit) + matrice[i - 1]
-                [w - int(list_actions[i-1].action_price)], matrice[i-1][w])
+                [w - int(list_actions[i-1][1])], matrice[i-1][w])
             else:
                 matrice[i][w] = matrice[i-1][w]
 
@@ -76,9 +71,9 @@ def optimized_investment(max_invest, list_actions):
     actions_selection = []
 
     while w >= 0 and n >= 0:
-        if matrice[n][w] == matrice[n-1][w - int(list_actions[n-1].action_price)] + int(list_actions[n-1].action_benefit):
+        if matrice[n][w] == matrice[n-1][w - int(list_actions[n-1][1])] + int(list_actions[n-1][3]):
             actions_selection.append(list_actions[n-1])
-            w -= int(list_actions[n-1].action_price)
+            w -= int(list_actions[n-1][1])
         n -= 1
     return matrice[-1][-1], actions_selection
 
@@ -86,12 +81,12 @@ def optimized_investment(max_invest, list_actions):
 if __name__ == '__main__':
 
 
-    #choice = file_choice()
-    start_time = time.time()
     list_actions, max_invest = transform_csv_to_obj(file_choice())
+    start_time = time.time()
     optimized_investment(max_invest, list_actions)
     actions_selection = optimized_investment(max_invest, list_actions)
-    print(actions_selection[0] / 100)
+    #print(actions_selection[0] / 100)
+    print(actions_selection)
     for x in actions_selection[1]:
-        print(x.__dict__)
+        print(x)
     print("--- %s secondes ---" % (time.time() - start_time))
